b996cf7a04c131f2d0d12452213e4feff896b6f3
Simon Epskamp <simon@frontwise.com>
2019-01-23 15:27:41 +0100

    Initial commit

---
+++ src/js/index.tsx
@@ -0,0 +1,34 @@
+console.log('Hello from typescript!');
+
+var h1 = document.createElement('h1');
+h1.innerText = 'Hello from typescript!';
+document.body.appendChild(h1);
+
+// React example
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+// // IE11 polyfills
+// import 'es6-shim';
+// import 'dom4';
+// import 'whatwg-fetch';
+
+type Props = { name: string };
+
+type State = {};
+
+class AppView extends React.Component<Props, State> {
+  render() {
+    return (
+      <h1>
+        Hello {this.props.name}!
+        <img src="/img/edit.svg" alt="edit img" />
+      </h1>
+    );
+  }
+}
+
+ReactDOM.render(
+  <AppView name="simon2" />,
+  document.getElementById('react-root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,117 @@
+const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+// Pass in public path with npm build --publicpath="http://example.com/assets"
+let publicPath = process.env.npm_config_publicpath;
+
+module.exports = env => {
+  var production = env && env.production;
+  var enableSourcemap = true; // Gives componentDidMountProblems
+
+  var config = {
+    entry: {
+      index: './src/js/index.tsx'
+    },
+    stats: { modules: false },
+    target: 'web',
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            production
+              ? { loader: MiniCssExtractPlugin.loader }
+              : // creates style nodes from JS strings
+                {
+                  loader: 'style-loader',
+                  options: { sourceMap: enableSourcemap }
+                },
+            {
+              loader: 'css-loader',
+              options: { url: false, sourceMap: enableSourcemap }
+            },
+            { loader: 'sass-loader', options: { sourceMap: enableSourcemap } }
+          ]
+        },
+        {
+          test: /\.(svg|png|jpg|jpeg|mp3)?$/,
+          loader: 'file-loader'
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js']
+    },
+    output: {
+      filename: '[name].[hash].js'
+    },
+    plugins: [
+      // Copy index.html, parse template tags
+      new HtmlWebpackPlugin({
+        inject: false,
+        template: './src/index.html',
+        filename: 'index.html'
+      })
+    ]
+  };
+
+  if (publicPath) {
+    config.output.publicPath = publicPath;
+  }
+
+  if (production) {
+    // prod
+    Object.assign(config, {
+      mode: 'production'
+    });
+
+    config.plugins.push(
+      // Set NODE_ENV to production for optimized react builds
+      new webpack.DefinePlugin({
+        'process.env': { NODE_ENV: JSON.stringify('production') }
+      }),
+      // Remove build dir
+      new CleanWebpackPlugin(['dist/*']),
+      // Extract css to file
+      new MiniCssExtractPlugin({
+        filename: '[name].[contenthash].css'
+      }),
+      // Copy images to build dir
+      new CopyWebpackPlugin([{ from: 'resources' }])
+    );
+  } else {
+    // dev
+    config.plugins.push(
+      // Hot module replacement
+      new webpack.HotModuleReplacementPlugin()
+    );
+
+    Object.assign(config, {
+      mode: 'development',
+      devtool: 'cheap-module-source-map',
+      devServer: {
+        contentBase: path.join(__dirname, '/resources/'),
+        overlay: {
+          errors: true,
+          warnings: false,
+          open: true
+        },
+        stats: { modules: false },
+        hot: true,
+        host: '0.0.0.0'
+        // https: true,
+      }
+    });
+  }
+
+  return config;
+};
